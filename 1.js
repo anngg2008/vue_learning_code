2038285682c1b2150494db288428c1f350ce82d2
ZHY <anngg2008@qq.com>
2020-06-21 13:04:02 +0800

    init

---
+++ 1.js
@@ -0,0 +1,21 @@
+// import Vue from 'vue'
+
+let app = new Vue({
+    el: "#app",
+    data: {
+        vm: {
+            title : "テスト画面",
+            article_title: "vtber",
+            article_content: [
+                { text: "有栖マナ" },
+                { text: "白上吹雪" },
+                { text: "神楽メア" },
+                { text: "湊あくあ" },
+                { text: "花丸晴琉" },
+                { text: "兎田ペコラ" },
+                { text: "陽向心美" },
+                { text: "花園セレナ" },
+            ],
+        }
+    },
+});
