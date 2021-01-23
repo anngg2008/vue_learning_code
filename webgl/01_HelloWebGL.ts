ab5e4b15d1444b91bc7b818fc1fb07c4ce3bf7a0
ZHY <anngg2008@qq.com>
2021-01-23 22:52:40 +0800

    upd

--- webgl/01_HelloWebGL.ts
+++ webgl/01_HelloWebGL.ts
@@ -1,3 +1,8 @@
+let angleX = 0.0;
+let angleY = 0.0;
+let angleZ = 0.0;
+let PER_ANGLE = Math.PI / 180.0;
+
 function main() {
     let canvas = document.getElementById("my_canvas") as HTMLCanvasElement;
     let gl = canvas.getContext("webgl");
@@ -5,11 +10,10 @@ function main() {
     let vert_src = document.getElementById("vertex-shader").innerText;
     let frag_src = document.getElementById("fragment-shader").innerText;
 
-    
     const verties = new Float32Array([
         -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
         0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-        0.0, 0.5, 0.0, 1.0, 0.0, 1.0,
+        0.0, 0.5, 0.0, 0.0, 0.0, 1.0,
     ]);
     const BYTES = verties.BYTES_PER_ELEMENT;
     
@@ -17,16 +21,25 @@ function main() {
     gl.useProgram(prog);
 
     let vbo = initVBO(gl, verties);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
 
     let pos_layout = gl.getAttribLocation(prog, "pos");
     gl.vertexAttribPointer(pos_layout, 3, gl.FLOAT, false, 6 * BYTES, 0 * BYTES);
     gl.enableVertexAttribArray(pos_layout);
 
-    let color_layout = gl.getAttribLocation(prog, "pos");
+    let color_layout = gl.getAttribLocation(prog, "color");
     gl.vertexAttribPointer(color_layout, 3, gl.FLOAT, false, 6 * BYTES, 3 * BYTES);
     gl.enableVertexAttribArray(color_layout);
 
+    let mvp = gl.getUniformLocation(prog, "mvp");
+    let mvp_mat = mat4.identity();
+    
+    angleY += PER_ANGLE * 10;
+
     setInterval(() => {
+        // 2nd openGL is column first.
+        mvp_mat = mat4.rotate(angleX, angleY, angleZ).dot(mvp_mat);
+        gl.uniformMatrix4fv(mvp, false, mvp_mat.toFloat32Array());
         gl.drawArrays(gl.TRIANGLES, 0, 3);
     }, 1000 / 16.67);
 }
--- webgl/matrix_utils.ts
+++ webgl/matrix_utils.ts
@@ -23,8 +23,39 @@ class mat4 {
         ]);
     }
 
+    static rotateX(x: number): mat4 {
+        return new mat4([
+            1.0, 0.0, 0.0, 0.0,
+            0.0, Math.cos(x), -Math.sin(x), 0.0,
+            0.0, Math.sin(x), Math.cos(x), 0.0,
+            0.0, 0.0, 0.0, 1.0
+        ]);
+    }
+
+    static rotateY(y: number): mat4 {
+        return new mat4([
+            Math.cos(y), 0.0, Math.sin(y), 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            -Math.sin(y), 0.0, Math.cos(y), 0.0,
+            0.0, 0.0, 0.0, 1.0
+        ]);
+    }
+
+    static rotateZ(z: number): mat4 {
+        return new mat4([
+            Math.cos(z), -Math.sin(z), 0.0, 0.0,
+            Math.sin(z), Math.cos(z), 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, 0.0, 0.0, 1.0
+        ]);
+    }
+
+    static rotate(x: number, y: number, z: number) {
+        return this.rotateZ(z).dot(this.rotateY(y)).dot(this.rotateX(x));
+    }
+
     row(index: number): Array<GLfloat> {
-        if(0 <= index && index < 4) {
+        if (0 <= index && index < 4) {
             let ret_data = this._data.filter((_, i) => {
                 return (i >= 4 * index) && (i < 4 * (index + 1));
             });
@@ -32,11 +63,11 @@ class mat4 {
             return ret_data;
         }
 
-        return null;
+        return [];
     }
 
     col(index: number): Array<GLfloat> {
-        if(0 <= index && index < 4) {
+        if (0 <= index && index < 4) {
             let ret_data = this._data.filter((_, i) => {
                 return i % 4 == index;
             });
@@ -44,44 +75,40 @@ class mat4 {
             return ret_data;
         }
 
-        return null;
+        return [];
     }
 
-    addNumber(rhs: GLfloat): mat4 {
-        let ret_data = this._data.map(v => { return v + rhs; });
-
-        return new mat4(ret_data);
-    }
-
-    addMat4(rhs: mat4): mat4 {
+    add(rhs: mat4 | number): mat4 {
         let ret_data = new Array<GLfloat>();
 
-        Array.from({ length: 16 }, (_, k) => { return k; }).forEach(index => {
-            ret_data.push(this._data[index] + rhs._data[index]);
-        })
-
-        return new mat4(ret_data);
-    }
-
-    dotMat4(rhs: mat4): mat4 {
-        let ret_data = new Array<GLfloat>();
-
-        Array.from({ length: 4 }, (_, k) => { return k; }).forEach(r => {
-            Array.from({ length: 4 }, (_, k) => { return k; }).forEach(c => {
-                let sum = Array.from({ length: 4 }, (_, k) => { return k; }).map(i => {
-                    return this.row(r)[i] * rhs.col(c)[i];
-                }).reduce((total, v) => { return total + v; }, 0);
-
-                ret_data.push(sum);
+        if (rhs instanceof Number) {
+            this._data.forEach((e) => { return e + <number>rhs.valueOf(); });
+        } else if (rhs instanceof mat4) {
+            Array.from({ length: 16 }, (_, k) => { return k; }).forEach(index => {
+                ret_data.push(this._data[index] + rhs._data[index]);
             });
-        });
+        }
 
         return new mat4(ret_data);
     }
 
-    dotNumber(rhs: GLfloat): mat4 {
-        let ret_data = this._data.map(v => { return v * rhs; });
+    dot(rhs: mat4 | number): mat4 {
+        let ret_data = new Array<GLfloat>();
 
+        if (rhs instanceof Number) {
+            ret_data = this._data.map((v) => { return v * <number>rhs.valueOf(); });
+        } else if (rhs instanceof mat4) {
+            Array.from({ length: 4 }, (_, k) => { return k; }).forEach(r => {
+                Array.from({ length: 4 }, (_, k) => { return k; }).forEach(c => {
+                    let sum = Array.from({ length: 4 }, (_, k) => { return k; }).map(i => {
+                        return this.row(r)[i] * rhs.col(c)[i];
+                    }).reduce((total, v) => { return total + v; }, 0);
+
+                    ret_data.push(sum);
+                });
+            });
+        }
+        
         return new mat4(ret_data);
     }
 
